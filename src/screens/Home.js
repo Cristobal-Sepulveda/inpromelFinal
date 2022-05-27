01316b1cd8d0778c0296b6fb1e4d3ada2f7f63b8
Cristobal Sepulveda <sepulveda.cristobal.ignacio@gmail.com>
2022-05-27 18:20:49 -0400

    se agrego userLocationButton

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,16 @@
-import React, {useState, useEffect, useContext, useRef} from 'react';
+import React, {useState, createRef} from 'react';
 import {View, Button, Text, StyleSheet, FlatList, SafeAreaView, TouchableOpacity, Image, Alert} from 'react-native';
 import BottomBar from '../components/BottomBar';
 import { connect } from 'react-redux';
 import Perfil from './Perfil';
 import Mapa from './Mapa';
 import ListadoDePendientes from './ListadoDePendientes';
-
+import { getLocationPermissions, getCurrentLocation } from '../utils/location';
 
 
 
 const Home = ({redux, wipeRedux}) => {
     const [showPerfilModal, setShowPerfilModal] = useState(false);
-
     const [isFocusedHome, setIsFocusedHome] = useState(true);
     const [showHome, setShowHome]= useState(true);
     const [coords, setCoords]= useState({
@@ -21,9 +20,45 @@ const Home = ({redux, wipeRedux}) => {
       longitudeDelta: 0.00421,
     });
 
+    const mapRef= createRef();
 
     const fabButtonPressed = () => {
       setShowHome(!showHome);
+    };
+
+
+    const backToUserLocation = async() => {
+      try {
+        const permission = await getLocationPermissions();
+        if (permission) {
+          try {
+              const { latitude, longitude } = await getCurrentLocation();
+              mapRef.current.animateToRegion({
+                latitude: latitude,
+                longitude: longitude,
+                latitudeDelta: 0.0001,
+                longitudeDelta: 0.00421
+              });
+          } catch {
+            Alert.alert("Hubo un error en cargar tu localización, se cargará otra localización por defecto.")
+            mapRef.current.animateToRegion({
+              latitude: 19.4978,
+              longitude: -99.1269,
+              latitudeDelta: 0.0001,
+              longitudeDelta: 0.00421
+            });            
+          }
+        } else {
+          mapRef.current.animateToRegion({
+            latitude: 19.4978,
+            longitude: -99.1269,
+            latitudeDelta: 0.0001,
+            longitudeDelta: 0.00421
+          });            
+        }
+      } catch (e) {
+          Alert.alert("Su celular presento problemas para solicitarle permisos, reinicie la app y vuelva a intentarlo.")
+      }
     };
     
     /** Esta función le da el source a la imagen del fabButton, segun sea el caso */
@@ -41,7 +76,12 @@ const Home = ({redux, wipeRedux}) => {
             <View style={styles.container}>
             <View style={styles.body}>
                 {showHome?(
-                  <Mapa coords={coords}/>            
+                  <>
+                    <Mapa mapRef={mapRef} coords={coords}/>            
+                    <TouchableOpacity style={styles.userLocationButton} onPress={()=> {backToUserLocation() }}>
+                      <Image source={require("../../assets/icons/userLocation.png")}/>
+                    </TouchableOpacity>      
+                  </>
                 ):(
                   <>
                     <ListadoDePendientes />
@@ -99,7 +139,18 @@ const styles = StyleSheet.create({
     backgroundColor:"#4285f4",
     borderRadius:50,
   },
-
+  userLocationButton:{
+    flex:1,
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 30,
+    bottom: 100,
+    backgroundColor:"#4285f4",
+    borderRadius:50,
+  },
       modalView: {
         backgroundColor: 'white',
         borderRadius: 10,
--- src/screens/Mapa.js
+++ src/screens/Mapa.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
-import MapView, {ProviderPropType, MAP_TYPES} from 'react-native-maps';
-
-const Mapa = ({coords}) => {
+import MapView, {ProviderPropType, MAP_TYPES, PROVIDER_GOOGLE} from 'react-native-maps';
+import { Provider } from 'react-redux';
+import { getLocationPermissions, getCurrentLocation } from '../utils/location';
+const Mapa = ({mapRef, coords}) => {
     return (
-      <MapView style={styles.map}
+      <MapView ref={mapRef}
+               style={styles.map}
+               provider={PROVIDER_GOOGLE}
+               showsUserLocation={true}
+               showsMyLocationButton={false}
                mapType={MAP_TYPES.HYBRID}
                region={coords}
                initialRegion={coords}
-               showsUserLocation
-               showsMyLocationButton={false}>
+               >
+                 
       </MapView>
     )
 };
@@ -21,6 +26,7 @@ const styles = StyleSheet.create({
     map: {
       width: Dimensions.get('window').width,
       height: Dimensions.get('window').height,
+      zIndex:-1,
     },
   });
 
